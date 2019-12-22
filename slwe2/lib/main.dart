e7063af3974a388d119c932b3ec04dcbc50862db
Unknown <leo5j472421@yahoo.com.tw>
2019-12-22 17:39:01 +0700

    temp Search Bar

--- slwe2/lib/main.dart
+++ slwe2/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
-void main(){
+
+void main() {
   runApp(new MaterialApp(
     home: new MyApp(),
   ));
 }
-
 
 class MyApp extends StatefulWidget {
   @override
@@ -16,20 +16,18 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return new SplashScreen(
-      seconds: 5,
-      navigateAfterSeconds: new AfterSplash(),
-      title: new Text('PokeDex',
-      style: new TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 20.0
-      ),),
-      image: Image(image: AssetImage('content/image/logo.jpeg')),
-      //new Image.asset('content/image/logo.jpeg'),
-      backgroundColor: Colors.white,
-      styleTextUnderTheLoader: new TextStyle(),
-      photoSize: 100.0,
-      loaderColor: Colors.red
-    );
+        seconds: 5,
+        navigateAfterSeconds: new AfterSplash(),
+        title: new Text(
+          'PokeDex',
+          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+        ),
+        image: Image(image: AssetImage('content/image/logo.jpeg')),
+        //new Image.asset('content/image/logo.jpeg'),
+        backgroundColor: Colors.white,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        loaderColor: Colors.red);
   }
 }
 
@@ -38,16 +36,126 @@ class AfterSplash extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-      //title: new Text("Welcome In SplashScreen Package"),
-      automaticallyImplyLeading: false
+        title: new Text("Welcome In SplashScreen Package"),
+        automaticallyImplyLeading: false,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                showSearch(context: context, delegate: SearchBarViewDelegate());
+              })
+        ],
       ),
       body: new Center(
-        child: new Text("Done!",
-        style: new TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 30.0
-        ),),
+        child: new Text(
+          "Done!",
+          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+        ),
+      ),
+    );
+  }
+}
 
+class SearchBarViewDelegate extends SearchDelegate<String> {
+  String searchHint = "GOGO Name";
+  var sourceList = [
+    "皮卡丘",
+    "皮卡丘1",
+    "皮卡丘2",
+    "皮卡丘3",
+    "皮卡丘4",
+  ];
+
+  var suggestList = [
+    "皮卡丘1",
+    "皮卡丘2",
+  ];
+
+  @override
+  String get searchFieldLabel => searchHint;
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    ///显示在最右边的控件列表
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = "";
+
+          ///搜索建议的内容
+          showSuggestions(context);
+        },
+      ),
+      IconButton(
+        icon: Icon(Icons.search),
+        onPressed: () => query = "",
+      )
+    ];
+  }
+
+  ///左侧带动画的控件，一般都是返回
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow, progress: transitionAnimation),
+
+      ///调用 close 关闭 search 界面
+      onPressed: () => close(context, null),
+    );
+  }
+
+  ///展示搜索结果
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> result = List();
+
+    ///模拟搜索过程
+    for (var str in sourceList) {
+      ///query 就是输入框的 TextEditingController
+      if (query.isNotEmpty && str.contains(query)) {
+        result.add(str);
+      }
+    }
+
+    ///展示搜索结果
+    return ListView.builder(
+      itemCount: result.length,
+      itemBuilder: (BuildContext context, int index) => ListTile(
+        title: Text(result[index]),
+      ),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> suggest = query.isEmpty
+        ? suggestList
+        : sourceList.where((input) => input.startsWith(query)).toList();
+    return ListView.builder(
+      itemCount: suggest.length,
+      itemBuilder: (BuildContext context, int index) => InkWell(
+        child: ListTile(
+          title: RichText(
+            text: TextSpan(
+              text: suggest[index].substring(0, query.length),
+              style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+              children: [
+                TextSpan(
+                  text: suggest[index].substring(query.length),
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+        ),
+        onTap: () {
+          //  query.replaceAll("", suggest[index].toString());
+          searchHint = "";
+          query = suggest[index].toString();
+          showResults(context);
+        },
       ),
     );
   }
